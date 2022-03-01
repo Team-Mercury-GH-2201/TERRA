a5dc2f387b32b5df3040c0a3ef6f193d4423aef0
Amber Nifong <amber.nifong@gmail.com>
2022-03-01 17:15:56 -0500

    Amber made an all plants front end component and set up a route for allPlants

--- client/Routes.js
+++ client/Routes.js
@@ -4,6 +4,7 @@ import {withRouter, Route, Switch, Redirect} from 'react-router-dom'
 import { Login, Signup } from './components/AuthForm';
 import Home from './components/Home';
 import {me} from './store'
+import {AllPlants} from './components/AllPlants';
 
 /**
  * COMPONENT
@@ -19,8 +20,10 @@ class Routes extends Component {
     return (
       <div>
         {isLoggedIn ? (
+          
           <Switch>
             <Route path="/home" component={Home} />
+            <Route path='/plant-friends' component={ AllPlants }/>
             <Redirect to="/home" />
           </Switch>
         ) : (
@@ -28,6 +31,7 @@ class Routes extends Component {
             <Route path='/' exact component={ Login } />
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
+            <Route path='/plant-friends' component={ AllPlants }/>
           </Switch>
         )}
       </div>
