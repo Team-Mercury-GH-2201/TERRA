9834aa49fdae029b3781dbf845e84cd98115ab29
Lauren Noble <lauren.noble3@gmail.com>
2022-03-02 12:52:25 -0500

    created and connected all users component, and tested backend api/users route

---
+++ client/store/users.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const SET_USERS = 'SET_USERS';
+
+const _setUsers = (users) => {
+    return {
+        type: SET_USERS,
+        users
+    }
+}
+
+export const fetchUsers = () => {
+    return async (dispatch) => {
+        try {
+            const { data } = await axios.get('/api/users')
+            dispatch(_setUsers(data))
+        } catch(err) {
+            console.error('error in fetchUsers thunk!', err)
+        }
+    }
+}
+
+export default function usersReducer(state=[], action) {
+    switch (action.type) {
+        case SET_USERS:
+            return action.users;
+        default:
+            return state;
+    }
+}
