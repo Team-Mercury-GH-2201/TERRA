c7459eae30deb4b91ae030808af59ff9c768462b
juhyeh <juhyeh@yahoo.com>
2022-03-02 12:17:35 -0500

    picture rendered in all plants

--- server/db/models/Plant.js
+++ server/db/models/Plant.js
@@ -23,7 +23,7 @@ const Plant = db.define('plant', {
     type: Sequelize.DECIMAL,
   },
   imageLink: {
-    type: Sequelize.STRING,
+    type: Sequelize.TEXT,
     defaultValue: 'https://lh3.googleusercontent.com/h_XMcnkmJ8YaF69F4rhKtFLtGVA3zauAlOcjJHfAn0kpo1UfIyyVym61rHfovPgFIQpiRWfRPcnokafO59Ad1MyC3FvuiSSrGSe5cVgNXhcjoOmTf_eDHOodChWKvX9Yor3NO_TX9A=w600-h315-p-k',
   },
 });
