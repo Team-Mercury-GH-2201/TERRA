1093ba6a3860ba5c1eefff1516e298cc9d069e1f
Lauren Noble <lauren.noble3@gmail.com>
2022-03-02 12:53:48 -0500

    fixed formatting

--- client/components/AllUsers.js
+++ client/components/AllUsers.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 import { fetchUsers } from '../store/users';
 
 export class AllUsers extends React.Component {
-    componentDidMount() {
-        this.props.fetchUsers();
-    }
-    render(){
-        return(
-            <div>
-                <h2>All Users</h2>
-            </div>
-        )
-    }
+  componentDidMount() {
+    this.props.fetchUsers();
+  }
+  render() {
+    return (
+      <div>
+        <h2>All Users</h2>
+      </div>
+    );
+  }
 }
 
 const mapDispatch = (dispatch) => {
-    return {
-        fetchUsers: () => dispatch(fetchUsers())
-    }
-}
+  return {
+    fetchUsers: () => dispatch(fetchUsers()),
+  };
+};
 
 export default connect(null, mapDispatch)(AllUsers);
