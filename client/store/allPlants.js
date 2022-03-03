a3d9373f4d9893056ff3be5fa3147713f61f27fc
Amber Nifong <amber.nifong@gmail.com>
2022-03-03 17:04:27 -0500

    delete functionality working front and back end

--- client/store/allPlants.js
+++ client/store/allPlants.js
@@ -3,6 +3,14 @@ import axios from 'axios';
 const SET_PLANTS = 'SET_PLANTS';
 const CREATE_PLANT = 'CREATE_PLANT';
 const EDIT_PLANT = 'EDIT_PLANT'
+const DELETE_PLANT = 'DELETE_PLANT'
+
+const _deletePlant = (plant) => {
+  return {
+    type: DELETE_PLANT,
+    plant
+  }
+}
 
 const _editPlant = (plant) => {
   return {
@@ -22,6 +30,14 @@ const _createPlant = (plant) => {
   return {
     type: CREATE_PLANT,
     plant,
+  };
+};
+
+export const deleteAPlant = (id, history) => {
+  return async (dispatch) => {
+    const { data } = await axios.delete(`/api/plant-friends/${id}`);
+    dispatch(_deletePlant(data));
+    history.push('/');
   };
 };
 
@@ -64,8 +80,10 @@ export default function plantsReducer(state = [], action) {
       return action.plants;
     case CREATE_PLANT:
       return [...state, action.plant];
-      case EDIT_PLANT:
-        return state.map((plant => plant.id === action.plant.id ? action.plant : plant));
+    case EDIT_PLANT:
+      return state.map((plant => plant.id === action.plant.id ? action.plant : plant));
+    case DELETE_PLANT:
+      return state.filter((plant) => plant.id !== action.plant.id);
     default:
       return state;
   }
