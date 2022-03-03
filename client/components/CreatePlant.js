941f77b43fba6b30aeb35fe28b4a1b10c9d4ee2f
Anna Hippee <anna.hippee@gmail.com>
2022-03-03 11:57:28 -0500

    progress on createPlant, added event handlers and updated state fields

--- client/components/CreatePlant.js
+++ client/components/CreatePlant.js
@@ -8,27 +8,38 @@ class CreatePlant extends React.Component {
             species: '',
             description: '',
             careInstructions: '',
-            price: null, //confirm what starting point should be here
+            imageLink: '',
+            price: 0, //confirm what starting point should be here
             errorMessage: ''
         }
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
-    handleChange(event){}
-    handleSubmit(event){}
+    handleChange(event){
+        this.setState({
+            [event.target.name]: event.target.value,
+        });
+    }
+    handleSubmit(event){
+        event.preventDefault();
+
+    }
     render() {
+        const { name, species, description, careInstructions, price, imageLink }
         return (
             <Form id="createPlantForm" onSubmit={this.handleSubmit} >
-                <label></label>
-                <input></input>
-                <label></label>
-                <input></input>
-                <label></label>
-                <input></input>
-                <label></label>
-                <input></input>
-                <label></label>
-                <input></input>
+                <label htmlFor="name">Name</label>
+                <input name="name" onChange={this.handleChange} value={name}></input>
+                <label htmlFor="species"></label>
+                <input name="species" onChange={this.handleChange} value={species}></input>
+                <label htmlFor="description"></label>
+                <input name="description" onChange={this.handleChange} value={description}></input>
+                <label htmlFor="careInstructions"></label>
+                <input name="careInstructions" onChange={this.handleChange} value={careInstructions}></input>
+                <label htmlFor="price"></label>
+                <input name="price" onChange={this.handleChange} value={price}></input>
+                <label htmlFor="imageLink"></label>
+                <input name="imageLink" onChange={this.handleChange} value={imageLink}></input>
             </Form>
         )
     }
