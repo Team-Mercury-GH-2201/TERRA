f1f60ec70025be08ef99874f44e374bca5293f3b
Anna Hippee <anna.hippee@gmail.com>
2022-03-08 09:39:34 -0500

    working set quantity for logged in users

--- server/api/cart.js
+++ server/api/cart.js
@@ -25,11 +25,10 @@ router.put('/add/:userId', async (req, res, next) => {
     let cartToAddTo = await Cart.findOne({
       where: {
         userId: req.params.userId,
-        isComplete: false,
       },
-      include: [Plant]
+      include: [Plant],
     });
-    if (!cartToAddTo || cartToAddTo.isComplete === true){
+    if (!cartToAddTo || cartToAddTo.isComplete === true) {
       cartToAddTo = await Cart.create({});
       cartToAddTo.setUser(req.params.userId);
     }
@@ -53,7 +52,7 @@ router.put('/remove/:userId', async (req, res, next) => {
       where: {
         userId: req.params.userId,
       },
-      include: [Plant]
+      include: [Plant],
     });
     await cartToAddTo.removePlant(req.body.id);
     const updatedCart = await Cart.findOne({
@@ -64,9 +63,9 @@ router.put('/remove/:userId', async (req, res, next) => {
     });
     res.json(updatedCart);
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 router.put('/checkout/:cartId', async (req, res, next) => {
   try {
@@ -75,46 +74,36 @@ router.put('/checkout/:cartId', async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-})
+});
 
 //  set quantity of logged in user cart
-// router.put('/:cartId/:userId', async (req, res, next) => {
-  // try {
-    // const plantAndCart = await Cart.findOne({
-      // where: {
-        // userId: req.params.userId,
-        // isComplete: false,
-      // },
-      // include: {
-        // model: Plant,
-        // through: {
-          // where: {
-            // plantId: req.body.plantId,
-          // },
-        // },
-      // },
-    // });
-    // const cart = await Cart.findOne({
-      // where: {
-        // userId: req.params.userId,
-        // isComplete: false,
-      // },
-    // });
-    // let plant = plantAndCart.plants[0];
-    // let plants = cart.plants
-    // let plantToBeUpdated = await plant.update( { ['plant-cart']: req.body});
-    // let plants = await cart.getPlants();
-    // console.log('CART', cart); // this is cart object that has cartId and userID
-    // console.log('CART AND PLANT', plantAndCart.plants[0]); // this is cart object with just one plant in array
-    // console.log('PLANTS', plants); // array of plants in cart
-    // await plantAndCart.setPlants([...plants, ...plant]], {
-      // through: { quantity: req.body.quantity },
-    // });
-// 
-    // res.json(plants);
-  // } catch (error) {
-    // next(error);
-  // }
-// });
+router.put('/:cartId', async (req, res, next) => {
+  try {
+    const plant = await Plant.findOne({
+      where: {
+        id: req.body.plantId,
+      },
+      include: {
+        model: Cart,
+        through: {
+          where: {
+            cartId: req.params.cartId,
+          },
+        },
+      },
+    });
+    const cart = await Cart.findOne({
+      where: { id: req.params.cartId, isComplete: false },
+    });
+    const result = await cart.addPlant(plant, { through: { quantity: req.body.quantity } });
+    const finalCart = await Cart.findOne({
+      where: { id: req.params.cartId, isComplete: false },
+      include: [Plant]
+    })
+    res.json(finalCart);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
