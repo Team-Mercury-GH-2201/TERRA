bfa8eb2bc85f151b3d279fb04c39a72069d47a93
juhyeh <juhyeh@yahoo.com>
2022-03-09 01:23:33 -0500

    more css

--- client/components/EditPlant.js
+++ client/components/EditPlant.js
@@ -1,20 +1,19 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { fetchPlant, updateAPlant } from '../store/singlePlant'
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchPlant, updateAPlant } from "../store/singlePlant";
 
 class EditPlant extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: '',
-      species: '',
-      description: '',
-      careInstructions: '',
-      imageLink: '',
-      price: '',
-      errorMessage: ''
-    }
+      name: "",
+      species: "",
+      description: "",
+      careInstructions: "",
+      imageLink: "",
+      price: "",
+      errorMessage: "",
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -24,78 +23,106 @@ class EditPlant extends Component {
     this.props.fetchPlant(id);
   }
 
-
   componentDidUpdate(prevProps) {
     if (prevProps.plant.id !== this.props.plant.id) {
       this.setState({
-        name: this.props.plant.name || '',
-        species: this.props.plant.species || '',
-        description: this.props.plant.description || '',
-        careInstructions: this.props.plant.careInstructions || '',
-        imageLink: this.props.plant.imageLink || '',
-        price: this.props.plant.price || '',
-        errorMessage: ''
+        name: this.props.plant.name || "",
+        species: this.props.plant.species || "",
+        description: this.props.plant.description || "",
+        careInstructions: this.props.plant.careInstructions || "",
+        imageLink: this.props.plant.imageLink || "",
+        price: this.props.plant.price || "",
+        errorMessage: "",
       });
     }
   }
 
   handleChange(evt) {
     this.setState({
-      [evt.target.name]: evt.target.value
+      [evt.target.name]: evt.target.value,
     });
   }
 
   async handleSubmit(evt) {
     evt.preventDefault();
     if (!this.state.name || !this.state.species) {
-      this.setState({ errorMessage: 'You must enter a name and species!' });
+      this.setState({ errorMessage: "You must enter a name and species!" });
     } else {
       await this.props.updateAPlant({ ...this.props.plant, ...this.state });
     }
   }
 
   render() {
-    const { name, species, description, careInstructions, imageLink, price, errorMessage } = this.state;
+    const {
+      name,
+      species,
+      description,
+      careInstructions,
+      imageLink,
+      price,
+      errorMessage,
+    } = this.state;
     const { handleSubmit, handleChange } = this;
     return (
-      <div >
+      <div id="form">
         <h2>Edit Your Plant</h2>
-        <form id='edit-plant-form' onSubmit={handleSubmit}>
-          <label htmlFor='plantName'>Plant Friend Name:</label>
-          <input name='name' onChange={handleChange} value={name} />
+        <form id="createPlantForm" onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="plantName">Plant Friend Name:</label>
+            <input name="name" onChange={handleChange} value={name} />
+          </div>
 
-          <label htmlFor='species' >Species:</label>
-          <input name='species' onChange={handleChange} value={species} />
+          <div>
+            <label htmlFor="species">Species:</label>
+            <input name="species" onChange={handleChange} value={species} />
+          </div>
 
-          <label htmlFor='description'  >Description:</label>
-          <input name='description' onChange={handleChange} value={description} />
+          <div>
+            <label htmlFor="description">Description:</label>
+            <input
+              name="description"
+              onChange={handleChange}
+              value={description}
+            />
+          </div>
 
-          <label htmlFor='careInstructions' >Care Instructions:</label>
-          <input name='careInstructions' onChange={handleChange} value={careInstructions} />
+          <div>
+            <label htmlFor="careInstructions">Care Instructions:</label>
+            <input
+              name="careInstructions"
+              onChange={handleChange}
+              value={careInstructions}
+            />
+          </div>
 
-          <label htmlFor='imageLink' >Image Link:</label>
-          <input name='imageLink' onChange={handleChange} value={imageLink} />
+          <div>
+            <label htmlFor="imageLink">Image Link:</label>
+            <input name="imageLink" onChange={handleChange} value={imageLink} />
+          </div>
 
-          <label htmlFor='price' >Price:</label>
-          <input name='price' onChange={handleChange} value={price} />
-          <div style={{color: "red"}}>{errorMessage}</div>
-          <button type='submit'>Submit</button>
+          <div>
+            <label htmlFor="price">Price:</label>
+            <input name="price" onChange={handleChange} value={price} />
+          </div>
+
+          <div style={{ color: "red" }}>{errorMessage}</div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
         </form>
-        <form onSubmit={(ev) => ev.preventDefault()}>
-        </form>
-        
+        <form onSubmit={(ev) => ev.preventDefault()}></form>
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
-  plant: state.plant
+  plant: state.plant,
 });
 
 const mapDispatchToProps = (dispatch, { history }) => ({
   fetchPlant: (id) => dispatch(fetchPlant(id)),
-  updateAPlant: (plant) => dispatch(updateAPlant(plant, history))
+  updateAPlant: (plant) => dispatch(updateAPlant(plant, history)),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(EditPlant)
+export default connect(mapStateToProps, mapDispatchToProps)(EditPlant);
